b850689e79da2f645c67ade48ca4c371269da102
Spencer Smith <smithspencer817@gmail.com>
2020-05-05 17:54:35 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,16 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |str|
+    puts "Hello #{str}!"
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  i=1
+  array.each_with_index do |obj, i|
+    puts "#{i}. #{obj}"
+    i+=1
+  end
 end
